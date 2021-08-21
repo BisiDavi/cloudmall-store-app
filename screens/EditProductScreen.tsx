6099d7606858031ce33785332ec812d2e3b04308
BisiDavi <oludavidconnect@gmail.com>
2021-08-21 18:56:25 +0100

    edit product screen added

---
+++ screens/EditProductScreen.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import { Image, Button } from "react-native-elements";
+import pizza from "@assets/pizza.png";
+
+export default function EditProductScreen() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.productImage} source={pizza} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: 20,
+  },
+  productImage: {
+    height: 150,
+    width: 200,
+    marginBottom: 20,
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  inputLabel: {
+    padding: 0,
+    marginTop: -5,
+  },
+  inputContainer: {
+    padding: 0,
+    height: 35,
+    marginBottom: 0,
+  },
+  input: {
+    padding: 0,
+  },
+});
+
+type productType = {
+  label: string;
+  name: string;
+};

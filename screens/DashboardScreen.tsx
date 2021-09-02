0231b639b4203ac2ca5d19ae290b9575dc8f41b8
BisiDavi <oludavidconnect@gmail.com>
2021-09-02 15:25:57 +0100

    react chart added

--- screens/DashboardScreen.tsx
+++ screens/DashboardScreen.tsx
@@ -1,7 +1,15 @@
 import { RouteProp } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import * as React from "react";
-import { StyleSheet, View, Text, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  View,
+  KeyboardAvoidingView,
+  ScrollView,
+  Text,
+  Platform,
+  Dimensions,
+} from "react-native";
 import DashboardCard from "@components/DashboardCard";
 import dashboardContent from "@json/dashboard.json";
 import { BottomTabParamList } from "../customTypes";
@@ -26,36 +34,43 @@ type dashboardContentType = {
 
 export default function DashboardScreen({ navigation }: Props) {
   return (
-    <View style={styles.container}>
-      <View style={styles.dashboardCards}>
-        {dashboardContent.card.map((item: dashboardContentType, index) => (
-          <View style={styles.category} key={`${item.category}-${index}`}>
-            <Text style={styles.categoryText}>{item.category}</Text>
-            <View style={styles.row}>
-              {item.content.map((content, index) => (
-                <DashboardCard
-                  key={index}
-                  link={item.link}
-                  navigation={navigation}
-                  content={content}
-                />
-              ))}
-            </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      enabled={true}
+    >
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.dashboardCards}>
+            {dashboardContent.card.map((item: dashboardContentType, index) => (
+              <View style={styles.category} key={`${item.category}-${index}`}>
+                <Text style={styles.categoryText}>{item.category}</Text>
+                <View style={styles.row}>
+                  {item.content.map((content, index) => (
+                    <DashboardCard
+                      key={index}
+                      link={item.link}
+                      navigation={navigation}
+                      content={content}
+                    />
+                  ))}
+                </View>
+              </View>
+            ))}
           </View>
-        ))}
-      </View>
-      <View>
-        <Text>Statistics</Text>
-        <Text>Performance: Number of orders vs days.</Text>
-      </View>
-    </View>
+          <View>
+            <Text style={styles.categoryText}>Statistics</Text>
+            <Text>Performance: Number of orders vs days.</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    alignItems: "flex-start",
     // justifyContent: "center",
     padding: 20,
   },
@@ -65,7 +80,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     flexDirection: "row",
     margin: 0,
-    width:Dimensions.get("window").width * 0.875
+    width: Dimensions.get("window").width * 0.875,
   },
   dashboardCards: {
     display: "flex",
@@ -75,7 +90,7 @@ const styles = StyleSheet.create({
     fontFamily: "MontserratBold",
     fontSize: 14,
     lineHeight: 16,
-    marginBottom:10
+    marginBottom: 10,
   },
   category: {
     margin: 0,

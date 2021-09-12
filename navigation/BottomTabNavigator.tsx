3b9900d96c63dc3e273d34a4df28ea0cf28c10e9
BisiDavi <oludavidconnect@gmail.com>
2021-09-12 01:55:58 +0100

    tab navigator updated

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -3,10 +3,9 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StyleSheet } from "react-native";
 import { BottomTabParamList } from "../customTypes";
 import {
-  TabOneNavigator,
-  TabTwoNavigator,
-  TabThreeNavigator,
-  TabFourNavigator,
+    TabOneNavigator,
+    TabTwoNavigator,
+    TabFourNavigator,
 } from "./TabNavigator";
 import colors from "@utils/colors";
 import DashboardSvg from "@assets/DashboardSvg";
@@ -20,76 +19,75 @@ import { Image } from "react-native-elements";
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
 export default function BottomTabNavigator() {
-  const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-  // useEffect(() => {
-  //   <WelcomeModal visible={showModal} onToggle={setShowModal} />;
-  // }, []);
+    // useEffect(() => {
+    //   <WelcomeModal visible={showModal} onToggle={setShowModal} />;
+    // }, []);
 
-  return (
-    <BottomTab.Navigator
-      screenOptions={{
-        headerShown: false,
-        headerLeft: () => (
-          <Image style={styles.menu} source={displayAsset("menuIcon")} />
-        ),
-        headerRight: () => (
-          <Image
-            style={styles.notificationIcon}
-            source={displayAsset("notificationIcon")}
-          />
-        ),
-      }}
-      initialRouteName="Orders"
-    >
-      <BottomTab.Screen
-        name="Dashboard"
-        options={{
-          tabBarActiveTintColor: colors.mallBlue5,
-          tabBarIcon: ({ color }: any) => <DashboardSvg color={color} />,
-        }}
-        component={TabTwoNavigator}
-      />
+    return (
+        <BottomTab.Navigator
+            screenOptions={{
+                headerShown: false,
+                headerLeft: () => (
+                    <Image
+                        style={styles.menu}
+                        source={displayAsset("menuIcon")}
+                    />
+                ),
+                headerRight: () => (
+                    <Image
+                        style={styles.notificationIcon}
+                        source={displayAsset("notificationIcon")}
+                    />
+                ),
+            }}
+            initialRouteName="Orders"
+        >
+            <BottomTab.Screen
+                name="Dashboard"
+                options={{
+                    tabBarActiveTintColor: colors.mallBlue5,
+                    tabBarIcon: ({ color }: any) => (
+                        <DashboardSvg color={color} />
+                    ),
+                }}
+                component={TabTwoNavigator}
+            />
 
-      <BottomTab.Screen
-        name="Orders"
-        options={{
-          tabBarActiveTintColor: colors.mallBlue5,
-          tabBarInactiveTintColor: colors.neutral5,
-          tabBarIcon: ({ color }: any) => <OrderSvg color={color} />,
-        }}
-        component={TabOneNavigator}
-      />
+            <BottomTab.Screen
+                name="Orders"
+                options={{
+                    tabBarActiveTintColor: colors.mallBlue5,
+                    tabBarInactiveTintColor: colors.neutral5,
+                    tabBarIcon: ({ color }: any) => <OrderSvg color={color} />,
+                }}
+                component={TabOneNavigator}
+            />
 
-      {/* <BottomTab.Screen
-        name="Products"
-        options={{
-          tabBarActiveTintColor: colors.mallBlue5,
-          tabBarIcon: ({ color }: any) => <StoreSvg color={color} />,
-        }}
-        component={TabThreeNavigator}
-      /> */}
-      <BottomTab.Screen
-        name="Profile"
-        options={{
-          tabBarActiveTintColor: colors.mallBlue5,
-          tabBarIcon: ({ color }: any) => <ProfileSvg color={color} />,
-        }}
-        component={TabFourNavigator}
-      />
-    </BottomTab.Navigator>
-  );
+            <BottomTab.Screen
+                name="Profile"
+                options={{
+                    tabBarActiveTintColor: colors.mallBlue5,
+                    tabBarIcon: ({ color }: any) => (
+                        <ProfileSvg color={color} />
+                    ),
+                }}
+                component={TabFourNavigator}
+            />
+        </BottomTab.Navigator>
+    );
 }
 
 const styles = StyleSheet.create({
-  menu: {
-    height: 20,
-    width: 20,
-    marginLeft: 20,
-  },
-  notificationIcon: {
-    marginRight: 20,
-    height: 20,
-    width: 20,
-  },
+    menu: {
+        height: 20,
+        width: 20,
+        marginLeft: 20,
+    },
+    notificationIcon: {
+        marginRight: 20,
+        height: 20,
+        width: 20,
+    },
 });
--- navigation/TabNavigator.tsx
+++ navigation/TabNavigator.tsx
@@ -11,121 +11,124 @@ const TabThreeStack = createStackNavigator();
 const TabFourStack = createStackNavigator();
 
 export function TabOneNavigator() {
-  return (
-    <TabOneStack.Navigator
-      screenOptions={{
-        headerShown: true,
-        headerTitleAlign: "center",
-        headerLeft: () => (
-          <Image style={styles.menu} source={displayAsset("menuIcon")} />
-        ),
-        headerRight: () => (
-          <Image
-            style={styles.notificationIcon}
-            source={displayAsset("notificationIcon")}
-          />
-        ),
-      }}
-    >
-      {bottomTabContent.tabOne.map((tab, index) => (
-        <TabOneStack.Screen
-          name={tab.name}
-          key={index}
-          options={{
-            headerTitleStyle: {
-              fontSize: 16,
-              textAlign: "left",
-            },
-            title: tab.title,
-          }}
-          component={displayScreenComponent(tab.name)}
-        />
-      ))}
-    </TabOneStack.Navigator>
-  );
+    return (
+        <TabOneStack.Navigator
+            screenOptions={{
+                headerShown: true,
+                headerTitleAlign: "center",
+                headerLeft: () => (
+                    <Image
+                        style={styles.menu}
+                        source={displayAsset("menuIcon")}
+                    />
+                ),
+                headerRight: () => (
+                    <Image
+                        style={styles.notificationIcon}
+                        source={displayAsset("notificationIcon")}
+                    />
+                ),
+            }}
+        >
+            {bottomTabContent.tabOne.map((tab, index) => (
+                <TabOneStack.Screen
+                    name={tab.name}
+                    key={index}
+                    options={{
+                        headerTitleStyle: {
+                            fontSize: 16,
+                            textAlign: "left",
+                        },
+                        title: tab.title,
+                    }}
+                    component={displayScreenComponent(tab.name)}
+                />
+            ))}
+        </TabOneStack.Navigator>
+    );
 }
 
 const TabTwoStack = createStackNavigator();
 
 export function TabTwoNavigator() {
-  return (
-    <TabTwoStack.Navigator
-      screenOptions={{
-        headerShown: false,
-        headerLeft: () => (
-          <Image style={styles.menu} source={displayAsset("menuIcon")} />
-        ),
-        headerRight: () => (
-          <Image
-            style={styles.notificationIcon}
-            source={displayAsset("notificationIcon")}
-          />
-        ),
-      }}
-    >
-      {bottomTabContent.tabTwo.map((tab, index) => (
-        <TabTwoStack.Screen
-          name={tab.name}
-          key={index}
-          options={{
-            headerShown: true,
-            headerTitleAlign: "center",
-            title: tab.title,
-          }}
-          component={displayScreenComponent(tab.name)}
-        />
-      ))}
-    </TabTwoStack.Navigator>
-  );
-}
-
-export function TabThreeNavigator() {
-  return (
-    <TabThreeStack.Navigator screenOptions={{ headerShown: false }}>
-      {bottomTabContent.tabThree.map((tab, index) => (
-        <TabThreeStack.Screen
-          name={tab.name}
-          key={index}
-          options={{
-            headerShown: true,
-            headerTitleAlign: "center",
-            title: tab.title,
-          }}
-          component={displayScreenComponent(tab.name)}
-        />
-      ))}
-    </TabThreeStack.Navigator>
-  );
+    return (
+        <TabTwoStack.Navigator
+            screenOptions={{
+                headerShown: false,
+                headerLeft: () => (
+                    <Image
+                        style={styles.menu}
+                        source={displayAsset("menuIcon")}
+                    />
+                ),
+                headerRight: () => (
+                    <Image
+                        style={styles.notificationIcon}
+                        source={displayAsset("notificationIcon")}
+                    />
+                ),
+            }}
+        >
+            {bottomTabContent.tabTwo.map((tab, index) => (
+                <TabTwoStack.Screen
+                    name={tab.name}
+                    key={index}
+                    options={{
+                        headerShown: true,
+                        headerTitleAlign: "center",
+                        title: tab.title,
+                    }}
+                    component={displayScreenComponent(tab.name)}
+                />
+            ))}
+        </TabTwoStack.Navigator>
+    );
 }
 
 export function TabFourNavigator() {
-  return (
-    <TabFourStack.Navigator screenOptions={{ headerShown: false }}>
-      {bottomTabContent.tabFour.map((tab, index) => (
-        <TabFourStack.Screen
-          name={tab.name}
-          key={index}
-          options={{
-            headerShown: true,
-            headerTitleAlign: "center",
-            title: tab.title,
-          }}
-          component={displayScreenComponent(tab.name)}
-        />
-      ))}
-    </TabFourStack.Navigator>
-  );
+    return (
+        <TabFourStack.Navigator
+            screenOptions={{
+                headerShown: false,
+                headerLeft: () => (
+                    <Image
+                        style={styles.menu}
+                        source={displayAsset("menuIcon")}
+                    />
+                ),
+                headerRight: () => (
+                    <Image
+                        style={styles.notificationIcon}
+                        source={displayAsset("notificationIcon")}
+                    />
+                ),
+            }}
+        >
+            {bottomTabContent.tabFour.map((tab, index) => (
+                <TabFourStack.Screen
+                    name={tab.name}
+                    key={index}
+                    options={{
+                        headerShown: true,
+                        headerTitleAlign: "center",
+                        title: tab.title,
+                    }}
+                    component={displayScreenComponent(tab.name)}
+                />
+            ))}
+        </TabFourStack.Navigator>
+    );
 }
 
 const styles = StyleSheet.create({
-  menu: {
-    height: 20,
-    width: 20,
-    marginLeft: 20,
-  },
-  notificationIcon: {
-    marginRight: 20,
-    height: 20,
-    width: 20,
-  },
+    menu: {
+        height: 20,
+        width: 20,
+        marginLeft: 20,
+    },
+    notificationIcon: {
+        marginRight: 20,
+        height: 20,
+        width: 20,
+    },
 });
